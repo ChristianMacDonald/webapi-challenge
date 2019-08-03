bf69406e39f858594fd37f4d73eba32c27dc4f23
ChristianMacDonald <chrismacd412@gmail.com>
2019-08-03 14:28:12 -0400

    Added actionRouter with all necessary requests for /api/actions endpoints

--- apiRouter.js
+++ apiRouter.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const projectRouter = require('./projectRouter');
+const actionRouter = require('./actionRouter');
 
 router.use('/projects', projectRouter);
+router.use('/actions', actionRouter);
 
 module.exports = router;
