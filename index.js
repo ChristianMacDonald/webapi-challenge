c8807d2526e6abc70efdf0f1dda4f014c9e355e1
ChristianMacDonald <chrismacd412@gmail.com>
2019-08-03 13:05:39 -0400

    Added nodemon as a dependency and added boilerplate for index.js

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+const express = require('express');
+
+const server = express();
+
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`API running on port ${port}`);
+});
